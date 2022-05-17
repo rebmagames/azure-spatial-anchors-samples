0b9a489289a4f9bd8ba7bb062e2c0134ed520cde
rebmagames <amber@voskamp.tv>
2022-05-17 15:34:54 +0200

    ASA basics
    ASA basics script added

---
+++ Unity/Assets/AzureSpatialAnchors.Examples/Scripts/ASABasics.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+///-------------------------------------------------------------------------------
+/// Author: Amber Voskamp
+/// <summary>
+/// Here comes the code for all the basic functionalities of ASA wich are now in AzurespatialAnchorsBasicDemoScript.cs and ASA find
+/// </summary>
+///-------------------------------------------------------------------------------
+
+public class ASABasics : MonoBehaviour
+{
+    
+}
