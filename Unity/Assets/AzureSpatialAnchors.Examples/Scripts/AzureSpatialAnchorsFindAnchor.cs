b3ec060dc3a96c10d32721644d2aad5428f74e0c
rebmagames <amber@voskamp.tv>
2022-05-18 14:34:23 +0200

    test
    test

--- Unity/Assets/AzureSpatialAnchors.Examples/Scripts/AzureSpatialAnchorsFindAnchor.cs
+++ Unity/Assets/AzureSpatialAnchors.Examples/Scripts/AzureSpatialAnchorsFindAnchor.cs
@@ -235,6 +235,8 @@ namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
 
         public async void test2()
         {
+            CleanupSpawnedObjects();
+            currentCloudAnchor = null;
             await StopSessionAsync();
         }
 
@@ -244,6 +246,7 @@ namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
             CloudManager.StopSession();
             await CloudManager.ResetSessionAsync();
             Debug.Log("!!! STOP");
+            RestartAndroid();
         }
 
         public void GetAnchorKey(string key)
@@ -266,7 +269,29 @@ namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
 
             SetAnchorIdsToLocate(anchorsToFind);
         }
+private static void RestartAndroid()
+    {
+        if (Application.isEditor) return;
 
+        using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            const int kIntent_FLAG_ACTIVITY_CLEAR_TASK = 0x00008000;
+            const int kIntent_FLAG_ACTIVITY_NEW_TASK = 0x10000000;
+
+            var currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            var pm = currentActivity.Call<AndroidJavaObject>("getPackageManager");
+            var intent = pm.Call<AndroidJavaObject>("getLaunchIntentForPackage", Application.identifier);
+
+            intent.Call<AndroidJavaObject>("setFlags", kIntent_FLAG_ACTIVITY_NEW_TASK | kIntent_FLAG_ACTIVITY_CLEAR_TASK);
+            currentActivity.Call("startActivity", intent);
+            currentActivity.Call("finish");
+            var process = new AndroidJavaClass("android.os.Process");
+            int pid = process.CallStatic<int>("myPid");
+            process.CallStatic("killProcess", pid);
+        }
     }
+    }
+
+    
 }
 
